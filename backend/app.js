02a1287095594f62088de9450cf805280dd41c7f
Abhinav <thenextabhinav>
2024-07-29 22:07:40 +0530

    server frontend backend

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./error/error.js";
 import reservationRouter from './route/reservationRoute.js';
+import path from 'path';
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
@@ -19,6 +20,14 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/reservation', reservationRouter);
+
+const __dirname = path.resolve(); // To resolve the current directory
+app.use(express.static(path.join(__dirname, 'public', 'dist')));
+
+// Catch-all handler to serve index.html for any other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'public', 'dist', 'index.html'));
+});
 
 dbConnection();
 
